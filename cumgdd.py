e80522d3408666377a20bf7a3a7eeedd57f0a166
Dawei Wang <daweiw@mun.ca>
2018-06-11 10:17:44 -0230

    Modified cumgdd.py

--- cumgdd.py
+++ cumgdd.py
@@ -8,22 +8,28 @@ This script calculates the cumulative GDD and
 save the results to a csv file.
 '''
 
-df = pd.read_csv(infile,index_col=0)
-date = df['Date/Time']
+if __name__ == "__main__":
+    tbase = int(sys.argv[2])
+    tupper = int(sys.argv[3])
+    infile = sys.argv[1]
+    
+#    pdb.set_trace()
+    
+    df = pd.read_csv(infile,index_col=0)
+    date = df['Date/Time']
+    
+    df.loc[df['Max_Temp']>tupper,'Max_Temp'] = tupper
+    df.loc[df['Min_Temp']<tbase,'Min_Temp'] = tbase
+    # Calculate daily growing degrees
+    gdd = ((df['Max_Temp'] + df['Min_Temp'])/2 - tbase).to_frame()
+    gdd.loc[gdd[0]<0, 0] = 0
+    # Compute cumulative GDD and reset index to date
+    cumgdd = gdd.cumsum()
+    cumgdd.columns = ['Cumulative GDD']
+    cumgdd.index = date
 
-df.loc[df['Max_Temp']>tupper,'Max_Temp'] = tupper
-df.loc[df['Min_Temp']<tbase,'Min_Temp'] = tbase
-# Calculate daily growing degrees
-gdd = ((df['Max_Temp'] + df['Min_Temp'])/2 - tbase).to_frame()
-gdd.loc[gdd[0]<0, 0] = 0
-
-# Compute cumulative GDD and reset index to date
-cumgdd = gdd.cumsum()
-cumgdd.columns = ['Cumulative GDD']
-cumgdd.index = date
-
-# Save GDD data
-year = df['Year'][0]
-prefix = infile.split('.')[0]
-outfile = prefix + '-CumGDD' + '.csv'
-cumgdd.to_csv(outfile, sep=',')
+    # Save GDD data
+    year = df['Year'][0]
+    prefix = infile.split('.')[0]
+    outfile = prefix + '_CumGDD' + '.csv'
+    cumgdd.to_csv(outfile, sep=',')
