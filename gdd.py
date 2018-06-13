c6519e5cc059b1983f6720d618b199dcebe9f372
Demarey Anthony Baker <dabaker@mun.ca>
2018-06-13 12:52:49 -0230

    Added Data/Time column to downloaded gdd data

--- gdd.py
+++ gdd.py
@@ -12,16 +12,18 @@ if __name__ == "__main__":
     tbase = int(sys.argv[2])
     tupper = int(sys.argv[3])
     infile = sys.argv[1]
-    
+
 #    pdb.set_trace()
-    
+
     df = pd.read_csv(infile,index_col=0)
     date = df['Date/Time']
-    
+
     df.loc[df['Max_Temp']>tupper,'Max_Temp'] = tupper
     df.loc[df['Min_Temp']<tbase,'Min_Temp'] = tbase
+
     # Calculate daily growing degrees
     gdd = ((df['Max_Temp'] + df['Min_Temp'])/2 - tbase).to_frame()
+    gdd['Data/Time'] = df['Date/Time']
     gdd.loc[gdd[0]<0, 0] = 0
 
     # Save GDD data
