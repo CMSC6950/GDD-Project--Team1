d9d2628c2c2cb6c231256719e105a8c67dbfcf75
Demarey Anthony Baker <dabaker@mun.ca>
2018-06-14 01:25:45 -0230

    Changed year to 2016

--- bokehplot.py
+++ bokehplot.py
@@ -15,19 +15,19 @@ gddHalifax =halifaxIntl.iloc[:][1].tolist()
 
 days = list(range(0,len(calgaryIntl)))
 
-halifaxIntlPlot = figure(title="GDD for Halifax for the year 2015", tools='hover, pan, box_zoom')
+halifaxIntlPlot = figure(title="GDD for Halifax for the year 2016", tools='hover, pan, box_zoom')
 halifaxIntlPlot.grid.grid_line_alpha=0.3
 halifaxIntlPlot.xaxis.axis_label = "Days"
 halifaxIntlPlot.yaxis.axis_label = "GDD"
 halifaxIntlPlot.line(days, gddHalifax, color='#000080')
 
-ottawaIntlPlot = figure(title="GDD for Ottawa for the year 2015", tools='hover, pan, box_zoom')
+ottawaIntlPlot = figure(title="GDD for Ottawa for the year 2016", tools='hover, pan, box_zoom')
 ottawaIntlPlot.grid.grid_line_alpha=0.3
 ottawaIntlPlot.xaxis.axis_label =  "Days"
 ottawaIntlPlot.yaxis.axis_label = "GDD"
 ottawaIntlPlot.line(days, gddOttawa, color='#33A02C')
 
-calgaryIntlPlot = figure(title="GDD for Calgary for the year 2015", tools='hover, pan, box_zoom')
+calgaryIntlPlot = figure(title="GDD for Calgary for the year 2016", tools='hover, pan, box_zoom')
 calgaryIntlPlot.grid.grid_line_alpha=0.3
 calgaryIntlPlot.xaxis.axis_label = "Days"
 calgaryIntlPlot.yaxis.axis_label = "GDD"
